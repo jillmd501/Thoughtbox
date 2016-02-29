fadb453acea21f9376bacc210866e156c0618947
Jill Donohue <jillmd501@gmail.com>
2016-02-29 11:47:30 -0700

    title editable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,4 +33,43 @@ function showLinks(link) {
     + "<h6 contenteditable='true' class='url-editable'>" + link.url + "</h6>"
     + "<input class='btn btn-default pull-right' id='read-" + link.read + "-button' type='button' name='submit' value='" + + "'></li>"
   )
+  searched();
 };
+
+function markAsRead(id) {
+  $('#mark-as-read' + id).on('click', function(){
+    event.preventDefault();
+
+    $.getJSON('/api/links/' + id, function(link){
+      var newStatus = function(){
+        if (link.read === 'read'){
+          return 'read'
+        } else { return 'unread'}
+      };
+
+      $.ajax({
+        type: 'PUT',
+        url: '/api/links/' + id + '.json',
+        data: {
+          link: {read: newStatus}
+        },
+        success: function(idea){
+          $('#mark-as-read' + id).html(newStatus);
+        }
+      })
+    })
+  })
+}
+
+function searchLinks() {
+  $("#filter").keyup(function(){
+		var filter = $(this).val();
+		$("#link-listing").children().each(function(){
+			if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+				$(this).fadeOut();
+			} else {
+				$(this).show();
+			}
+		});
+	});
+}
