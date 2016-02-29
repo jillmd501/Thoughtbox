db2cf783c9a0d2ab157c7697d8830ae040cf3efd
Jill Donohue <jillmd501@gmail.com>
2016-02-29 14:23:37 -0700

    added jquery to link.js file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     namespace :api do
        namespace :v1 do
-         resources :links, only: [:index, :update], defaults: { format: 'json' }
+         resources :links, only: [:index, :update, :destroy], defaults: { format: 'json' }
        end
      end
 
