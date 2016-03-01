ab7391c2eefeabb95ade6e6c6709617558e977df
Jill Donohue <jillmd501@gmail.com>
2016-02-29 17:42:43 -0700

    beefed up some testing

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "link is valid with correct attributes" do
+    link = Link.create(title: "test", url: "http://www.test.com")
+    assert_equal "http://www.test.com", link.url
+    assert_equal "test", link.title
+  end
+
+  test "link is invalid without title" do
+    link = Link.create(title: "", url: "http://www.test.com")
+    refute link.valid?
+  end
+
+  test "link is invalid without url" do
+    link = Link.create(title: "hi", url: "test.com")
+    refute link.valid?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'rails/test_help'
 require 'capybara/rails'
 
 class ActiveSupport::TestCase
+  def json_response
+    JSON.parse(response.body)
+  end
 end
 
 class ActionDispatch::IntegrationTest
